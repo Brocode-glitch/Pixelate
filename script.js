602263a5d1195f3d58862b5d18c07dd4beebd97f
Brocode-glitch <123038789+Brocode-glitch@users.noreply.github.com>
2023-03-06 11:12:28 -0500

    Updated Pixel Project Next step Change Event

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Your code here
+const table = document.getElementById("the-ul");
+const addRowButton = document.getElementById("add-row");
+addRowButton.addEventListener("click", makeRow);
+
+table.addEventListener("click", colorize);
+
+function makeRow() {
+  const newLi = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    newLi.appendChild(td);
+  }
+  table.appendChild(newLi);
+}
+
+function colorize(event) {
+  console.log("Clicked!");
+  const targetSquare = event.target;
+  if (targetSquare.className.length) {
+    targetSquare.className = "";
+  } else {
+    targetSquare.className = "red";
+  }
+}
